7ecb64f2842c35b3e23cb2b457cb51b5b11703e3
Samir Sadek <samirsadek2009@gmail.com>
2023-11-24 07:37:41 +0600

    Project setup with  tailwindcss and react router dom

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2 className='font-bold bg-green text-secondary border'>Hello From Samir</h2>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        "green" : "#37BA74",
+        "secondary" : "#555"
+      }
+    },
+  },
+  plugins: [],
+}
+
